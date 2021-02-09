2cc55d6bbb569607d3326a617cced655979a60e7
josemnr <maninari97@gmail.com>
2021-02-09 16:53:14 -0600

    ADD webserver and livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,50 @@
 const { series } = require('gulp');
 let gulp = require('gulp');
 let sass = require('gulp-sass');
-let typescript = require('gulp-typescript')
+let typescript = require('gulp-typescript');
+let webserver = require('gulp-webserver');
+
+const buildPath = 'dist';
 
 gulp.task('html', function(){
-    console.log('Tarea para el html')
     return gulp.src('src/index.html')
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(buildPath));
 });
+
+gulp.task('watch:html', gulp.series('html', function(done){
+    gulp.watch('src/**/*.html', gulp.series('html'));
+    done();
+}));
 
 gulp.task('styles', function(){
-    console.log('Tarea para transformar SASS')
     return gulp.src('src/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('dist/assets/styles'));
+    .pipe(gulp.dest(buildPath + '/assets/styles'));
 });
 
+gulp.task('watch:styles', gulp.series('styles', function(done){
+    gulp.watch('src/**/*.scss', gulp.series('styles'));
+    done();
+}));
+
 gulp.task('scripts', function(){
-    console.log('Tarea para transformar Typescript')
     let tsConfig = typescript.createProject('tsconfig.json')
     return tsConfig.src()
     .pipe(tsConfig())
-    .pipe(gulp.dest('dist/assets/scripts'));
+    .pipe(gulp.dest(buildPath + '/assets/scripts'));
 });
 
-gulp.task('default', gulp.series(['styles', 'scripts', 'html']));
+gulp.task('watch:scripts', gulp.series('scripts', function(done){
+    gulp.watch('src/**/*.ts', gulp.series('scripts'));
+    done();
+}));
+
+gulp.task('serve', function(){
+    return gulp.src(buildPath)
+    .pipe(webserver({
+        open: true,
+        livereload: true
+    }))
+});
+
+gulp.task('default', gulp.parallel('watch:styles', 'watch:scripts', 'watch:html', 'serve'));
