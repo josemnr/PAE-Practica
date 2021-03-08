65907aa6204bf11a72eaaa26e5a965e942c8527e
josemnr <maninari97@gmail.com>
2021-03-07 18:20:43 -0600

    Clean workspace

---
+++ index.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const express = require('express');
+const {newsRouter,userRouter} = require('./src/routes');
+const {config} = require('./config');
+const MongoClient = require('mongodb').MongoClient;
+// const { isIfStatement } = require('typescript');
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use('/assets', express.static(path.join(__dirname, './public')));
+app.set("views", "views")
+app.set("view engine", "hbs")
+
+app.use("/", newsRouter);
+app.use("/users", userRouter);
+
+app.listen(config.port, () => {
+    console.log(`The server is now running on Port ${config.port}`);
+});
+
+// MongoClient.connect(config.mongoUrl, { useUnifiedTopology: true }, (err, client) => {
+//     if(err){
+//         console.log('Failed to connect to MongoDB');
+//         return;
+//     }
+//     const db = client.db();
+//     console.log('Successfully connected to MongoDB');
+//     const collection = db.collection("users");
+//     collection.find({}).toArray((err, results) => {
+//         if(err){
+//             console.log("Could not retrive users", err);
+//             return;
+//         }
+//         console.log("Users: ", results);
+//     })
+// });
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -18,22 +18,4 @@ searcherButton.onclick = function () {
         });
         boxContainer.style.display = "flex";
     });
-
-    // let url = 'http://newsapi.org/v2/everything?' +
-    //     `q=${searcherInput.value}&` +
-    //     `from=${date}&` +
-    //     'sortBy=popularity&' +
-    //     'apiKey=2f2ef6f079f6471c9e88edf4e0d4d2e4';
-    // let req = new Request(url);
-    // fetch(req)
-    //     .then((res) => {
-    //     return res.json();
-    // }).then((data) => {
-    //     console.log(data);
-    //     document.getElementById('box--container').innerHTML = template({
-    //         news: data.articles,
-    //     });
-    //     boxContainer.style.display = "flex";
-    // });
-
 };
