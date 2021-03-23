62fdf7fa40dd9f01d45492d954b5e33a19292ca6
josemnr <maninari97@gmail.com>
2021-03-23 13:10:43 -0600

    Update config files - Tomar para P3

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,44 @@
-const { series } = require('gulp');
 let gulp = require('gulp');
 let sass = require('gulp-sass');
-let typescript = require('gulp-typescript');
 let webserver = require('gulp-webserver');
 
-const buildPath = 'dist';
+// gulp.task('hbs', function(){
+//     return gulp.src('views/**/*.hbs')
+//     .pipe(gulp.dest(buildPath + '/views'));
+// });
 
-gulp.task('html', function(){
-    return gulp.src('views/index.html')
-    .pipe(gulp.dest(buildPath + '/views'));
-});
-
-gulp.task('watch:html', gulp.series('html', function(done){
-    gulp.watch('views/**/*.html', gulp.series('html'));
-    done();
-}));
+// gulp.task('watch:hbs', gulp.series('hbs', function(done){
+//     gulp.watch('views/**/*.hbs', gulp.series('hbs'));
+//     done();
+// }));
 
 gulp.task('styles', function(){
-    return gulp.src('src/**/*.scss')
+    return gulp.src('src/scss/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest(buildPath + '/public/styles'));
+    .pipe(gulp.dest('public/styles'));
 });
 
 gulp.task('watch:styles', gulp.series('styles', function(done){
-    gulp.watch('src/**/*.scss', gulp.series('styles'));
+    gulp.watch('src/scss/*.scss', gulp.series('styles'));
     done();
 }));
 
-gulp.task('scripts', function(){
-    let tsConfig = typescript.createProject('tsconfig.json')
-    return tsConfig.src()
-    .pipe(tsConfig())
-    .pipe(gulp.dest(buildPath + '/public/scripts'));
-});
+// gulp.task('js', function(){
+//     return gulp.src('**/**/*.js')
+//     .pipe(gulp.dest(buildPath));
+// });
 
-gulp.task('watch:scripts', gulp.series('scripts', function(done){
-    gulp.watch('public/scripts/**/*.ts', gulp.series('scripts'));
-    done();
-}));
+// gulp.task('watch:js', gulp.series('js', function(done){
+//     gulp.watch('**/**/*.js', gulp.series('js'));
+//     done();
+// }));
 
-gulp.task('js', function(){
-    return gulp.src('src/app.js')
-    .pipe(gulp.dest(buildPath + '/src'));
-});
+// gulp.task('serve', function(){
+//     return gulp.src(buildPath + '/views/index.hbs')
+//     .pipe(webserver({
+//         open: true,
+//         livereload: true
+//     }))
+// });
 
-gulp.task('watch:js', gulp.series('js', function(done){
-    gulp.watch('src/**/*.js', gulp.series('js'));
-    done();
-}));
-
-gulp.task('serve', function(){
-    return gulp.src(buildPath + '/src/app.js')
-    .pipe(webserver({
-        open: true,
-        livereload: true
-    }))
-});
-
-gulp.task('default', gulp.series('watch:styles', 'watch:scripts', 'watch:html', 'watch:js', 'serve'));
+gulp.task('default', gulp.series('watch:styles'));
